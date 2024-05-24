8c0743b6b3cfe2e33f427f14b164c3457e1893f6
Kim-game3 <zweilous387@icloud.com>
2024-05-24 22:23:48 +0900

    回転の実装(Aで左、Dで右)
    まだコントローラーに対応できてないけどいったん上げます

---
+++ SWICTH_Prototype/Assets/Scripts/SetTriangleScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTriangleScript : MonoBehaviour
+{
+    [SerializeField] Transform center;//中心となるオブジェクト
+    [SerializeField] Transform[] vertices;//配置するオブジェクト
+    [SerializeField] float sideLength = 25f;//正三角形の一辺の長さ
+
+    void Start()
+    {
+        //この関数で正三角形の頂点を計算して配置する
+        Vector3[] positions = CalculateTriangleVertices(center.position, sideLength);
+
+        for(int i = 0; i < vertices.Length && i < positions.Length; i++)
+        {
+            vertices[i].position = positions[i];
+            vertices[i].parent = center;//ここで頂点のオブジェクト3つの親をcenterに設定
+        }
+    }
+
+    Vector3[] CalculateTriangleVertices(Vector3 center, float sideLength)
+    {
+        //高さを計算
+        float height = Mathf.Sqrt(3) / 2 * sideLength;
+
+        //頂点を計算
+        Vector3[] vertices = new Vector3[3];
+        vertices[0] = center + new Vector3(-sideLength / 2, 10, height / 3);
+        vertices[1] = center + new Vector3(sideLength / 2, 10, height / 3);
+        vertices[2] = center + new Vector3(0, 10, -2 * height / 3);
+
+        return vertices;
+    }
+}
