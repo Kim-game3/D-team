c354b3bbbfd9acac9eb7a4851cffa75df2796fb1
Yoshizawa-TPU <g2327080@st.t-kouge.ac.jp>
2024-08-23 03:41:24 +0900

    spawn

--- SWICTH_Prototype/Assets/Scripts/Spawn.cs
+++ SWICTH_Prototype/Assets/Scripts/Spawn.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    [SerializeField] GameObject seeds;
-    [SerializeField] GameObject spawnPoint;
+    [SerializeField] GameObject seeds;//種
+    [SerializeField] GameObject[] fieid;//畑の位置
+    [SerializeField] GameObject[] spawnSeeds;//格納場所
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(seeds,transform.position,transform.rotation);
+
+        for (int i = 0; i < fieid.Length; i++)
+        {
+            if (fieid[i] != null)
+            {
+                Vector3 spawnPosition = fieid[i].transform.position;
+                spawnSeeds[i] = Instantiate(seeds, spawnPosition, Quaternion.identity);
+            }
+        }
+
+        //Instantiate(seeds,transform.position,transform.rotation);
     }
 
     // Update is called once per frame
@@ -22,5 +33,7 @@ public class Spawn : MonoBehaviour
 
 /*
  * 空のオブジェクトの位置にseedsをスポーンさせる。
- * 
+ * fieldの要素数とspawnSeedsの要素数は同じにしてください。
+ * fieldにからのオブジェクトを入れてその位置にseedsがスポーンします。
+ * スポーンしたseedsはspawnSeedsに格納されます。
  */
