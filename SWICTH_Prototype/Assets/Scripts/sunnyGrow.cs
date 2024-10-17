1703e1a143f59b726fdbce64ec95c51ef0552cc6
Yoshizawa-TPU <g2327080@st.t-kouge.ac.jp>
2024-10-17 11:44:16 +0900

    当たり判定の修正

--- SWICTH_Prototype/Assets/Scripts/sunnyGrow.cs
+++ SWICTH_Prototype/Assets/Scripts/sunnyGrow.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class sunnyGrow : MonoBehaviour
 {
-    public GameObject[] bbb;//中に種が入ってます。
-    public GameObject seed;
+    public GameObject[] seeds;//中に種が入ってます。
+    public GameManager GM;
 
     int count;
 
@@ -23,10 +25,11 @@ public class sunnyGrow : MonoBehaviour
 
     public void sunnyGrowing()
     {
-        for (int i = 0; i < bbb.Length; i++)
+        Debug.Log("テスト１");//ここまで来てる。
+        for (int i = 0; i < seeds.Length; i++)
         {
+            
             count++;
-            seed = GameObject.Instantiate(bbb[count])as GameObject;
         }
 
     }
@@ -35,9 +38,12 @@ public class sunnyGrow : MonoBehaviour
     {
         if (other.CompareTag("Sunny"))
         {
+            for(int i = 0;i < seeds.Length;i++)
+            {
+                Debug.Log(seeds[i].name);
+            }
             Debug.Log("晴れ種成長します!");
-            Debug.Log(bbb[0].name);
-        }
+        }//サニーおけ
     }
 
 }
