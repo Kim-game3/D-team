6eb49b41d68efb2419e8683c1d92ccf314e0e8d7
Kim-game3 <zweilous387@icloud.com>
2024-10-22 15:06:42 +0900

    メインゲームのフレームレートを統一

--- SWICTH_Prototype/Assets/Scripts/SetTriangleScript.cs
+++ SWICTH_Prototype/Assets/Scripts/SetTriangleScript.cs
@@ -24,6 +24,7 @@ public class SetTriangleScript : MonoBehaviour
 
     void Start()
     {
+        Application.targetFrameRate = 60;
         Vector3[] positions = CalculateFiveVertices(sideLength);
 
         for (int i = 0; i < vertices.Length && i < positions.Length; i++)
