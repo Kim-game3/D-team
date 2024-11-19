eda1e2d2e4cc32ce0da0050809221af50216e0ab
Yoshizawa-TPU <g2327080@st.t-kougei.ac.jp>
2024-11-19 17:05:59 +0900

    タイトル画面の矢印キーでの選択

---
+++ SWICTH_Prototype/Assets/Scripts/inputButton.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class inputButton : MonoBehaviour
+{
+    private int selectIndex= 0;
+    public TextMeshProUGUI[] menuItems;
+    public Color normal = Color.white;
+    public Color select = Color.red;
+    [SerializeField] GameManager GM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateMenuVisuals();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        // 上キーで選択を上に移動
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectIndex = (selectIndex - 1 + menuItems.Length) % menuItems.Length;
+            UpdateMenuVisuals();
+        }
+
+        // 下キーで選択を下に移動
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectIndex = (selectIndex + 1) % menuItems.Length;
+            UpdateMenuVisuals();
+        }
+
+        // Enterキーで選択を確定
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            ExecuteSelectedOption();
+        }
+    }
+
+    void UpdateMenuVisuals()
+    {
+        for (int i = 0; i < menuItems.Length; i++)
+        {
+            if (i == selectIndex)
+            {
+                menuItems[i].color = select;
+            }
+            else
+            {
+                menuItems[i].color = normal;
+            }
+        }
+    }
+
+    void ExecuteSelectedOption()
+    {
+        switch (selectIndex)
+        {
+            case 0: // 「スタート」
+                Debug.Log("ゲームスタート");
+                GM.StartButton();
+                break;
+            case 1: // 「遊び方」
+                Debug.Log("遊び方を表示");
+                break;
+            case 2: // 「終わる」
+                Debug.Log("ゲーム終了");
+                GM.ExitButton();
+                break;
+        }
+    }
+}
